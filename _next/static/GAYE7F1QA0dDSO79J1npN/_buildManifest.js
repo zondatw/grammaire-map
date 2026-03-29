76bebddd7660af1a6ce8182196f35c9319babaa6
zonda yang <u226699@gmail.com>
2026-03-29 19:09:31 +0800

    deploy: fix mobile apostrophe in fill-in answers

--- _next/static/GAYE7F1QA0dDSO79J1npN/_buildManifest.js
+++ _next/static/GAYE7F1QA0dDSO79J1npN/_buildManifest.js
@@ -4,7 +4,7 @@ self.__BUILD_MANIFEST = {
     "beforeFiles": [
       {
         "source": "/.//_next/:path+",
-        "destination": "/grammaire-map/_next/:path+"
+        "destination": "/_next/:path+"
       }
     ],
     "fallback": []
